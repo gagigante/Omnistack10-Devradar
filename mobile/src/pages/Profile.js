a06d098621d5c9b41165ac8ad3f8aef1426bc19b
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-03-28 13:37:03 -0300

    Finished mobile app

---
+++ mobile/src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { WebView } from 'react-native-webview';
+
+// import { Container } from './styles';
+
+export default function Profile({ navigation }) {
+
+    const github_username = navigation.getParam('github_username');
+  return (
+    <WebView style={{ flex: 1 }} source={{ uri: `https://github.com/${github_username}`}} />
+  );
+}
